527e4783cfc677cb7e50930d2ebe05104b755c18
joseftango <4279@holbertonschool.com>
2022-04-28 15:15:53 +0100

    9-easter_egg.py

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("Beautiful is better than ugly.Explicit is better than implicit.Simple is better than complex.")
+print("Beautiful is better than ugly.\nExplicit is better than implicit.")
