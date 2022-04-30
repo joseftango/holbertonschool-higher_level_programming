60815d6eaafcef90f879c48b4546850abab7604b
joseftango <4279@holbertonschool.com>
2022-04-30 23:44:25 +0100

    12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    i = 1
+    for i in range(i, 101):
+        if i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        elif i % 5 == 0 and i % 3 == 0:
+            print("FizzBuzz", end=" ")
+        else:
+            print(f"{i}", end=" ")
