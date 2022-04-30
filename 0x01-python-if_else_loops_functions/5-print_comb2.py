e47f932cfe57a7f27bb6f0fae7238ea3a04fa3f9
joseftango <4279@holbertonschool.com>
2022-04-30 14:03:17 +0100

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if(i < 10):
-        i = "0" + str(i)
-    print(f"{i}", end=(", "))
     if i == 99:
-        print("\n")
+        print(f"{i}")
+        continue
+    print(f"{i:02d}", end=", ")
