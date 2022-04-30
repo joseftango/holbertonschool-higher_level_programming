33ec1bb153273efc535a2b15e0c77090cb4f7a30
joseftango <4279@holbertonschool.com>
2022-04-30 18:37:49 +0100

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for x in range(0, 100):
-    if x == 99:
-        print(f"{x}")
+    if (x == 89):
+        print(f"{x:02d}")
+        break
     if (x / 10) < (x % 10):
         print(f"{x:02d}", end=", ")
