0c0d3a366ad43f003df3951d6901614e6f73c852
joseftango <4279@holbertonschool.com>
2022-05-04 13:41:36 +0100

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == "":
+        sentence[0] = None
+    first_char = sentence[0]
+    len_sen = len(sentence)
+    my_tuple = (len_sen, first_char)
+    return my_tuple
