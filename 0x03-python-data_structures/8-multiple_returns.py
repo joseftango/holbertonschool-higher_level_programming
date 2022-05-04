dbd405836f4595cf814cdfff6e289e1ae0916e5b
joseftango <4279@holbertonschool.com>
2022-05-04 13:48:19 +0100

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,10 @@
 def multiple_returns(sentence):
     if sentence == "":
         sentence[0] = None
-        return len(sentence),sentence[0]
+        first_char = sentence[0]
+        len_sen = len(sentence)
+        my_tuple = (len_sen, first_char)
+        return my_tuple
     else:
         first_char = sentence[0]
         len_sen = len(sentence)
