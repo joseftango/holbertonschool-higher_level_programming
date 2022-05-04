7f0a3a0f67856bf5d28dd89cc5ae3284a25ff0ed
joseftango <4279@holbertonschool.com>
2022-05-04 14:26:26 +0100

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ from asyncio.windows_events import NULL
 
 
 def max_integer(my_list=[]):
-    if my_list == NULL:
+    if my_list == []:
         return None
     max = my_list[0]
     for x in my_list:
