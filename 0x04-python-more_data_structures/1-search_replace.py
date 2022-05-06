f532158c1a7cf94b4abfe3dcc882ab0945a7a403
joseftango <4279@holbertonschool.com>
2022-05-06 10:09:17 +0100

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     list = []
-    for i in range(len(my_list)):
-        if my_list[i] == search:
+    for i in my_list:
+        if i == search:
             list.append(replace)
         else:
             list.append(i)
