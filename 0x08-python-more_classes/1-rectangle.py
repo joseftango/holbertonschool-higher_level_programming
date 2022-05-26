bb738a39f3be5e9f68417ceaba74273e5104558e
joseftango <4279@holbertonschool.com>
2022-05-26 14:28:55 +0100

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-""" this class define a Rectangle"""
+""""this is a Rectangle"""
 
 
 class Rectangle:
-    """Rectangle."""
-
+    """"Rectangle class"""
     def __init__(self, width=0, height=0):
-        """Initializer"""
-        self.width = width
+        """inisialising"""
         self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -16,7 +15,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -28,7 +27,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
