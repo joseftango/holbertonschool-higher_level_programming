0e1baf113eb6256757158e87755db51daae38416
joseftango <4279@holbertonschool.com>
2022-05-30 18:05:38 +0100

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""this is a function that takes a class as argument"""
+
 
 def lookup(obj):
-    """ function that returns a list of available attributes and methods of an object"""
+    """this is a function that takes a class as argument"""
     return dir(obj)
