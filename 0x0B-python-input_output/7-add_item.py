6b9ba3deed6b4dc3487d6ff637ab12b74806bbc4
joseftango <4279@holbertonschool.com>
2022-06-03 19:59:03 +0100

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -15,6 +15,11 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 """import 6-load_from_json_file module"""
 
 
+try:
+    new_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    my_list = []
+
 my_list = load_from_json_file("add_item.json")
 
 for i in argv[1:]:
