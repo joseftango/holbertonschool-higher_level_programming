574d4f77079e306c29ef827e9bea202dc4033b6e
joseftango <4279@holbertonschool.com>
2022-06-03 19:48:09 +0100

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,8 +8,11 @@ from sys import argv
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+"""import 5-save_to_json_file module"""
+
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-"""import two modules"""
+"""import 6-load_from_json_file module"""
 
 
 my_list = load_from_json_file("add_item.json")
