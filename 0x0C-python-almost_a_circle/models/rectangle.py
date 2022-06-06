d5df79802916b192abdfd81ea0be322905f0fb47
joseftango <4279@holbertonschool.com>
2022-06-06 15:49:17 +0100

    rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""import a module"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """this is a class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def get_width(self):
+        return self.__width
+
+    def set_width(self, value):
+        self.__width = value
+
+    @property
+    def get_height(self):
+        return self.__height
+
+    def set_height(self, value):
+        self.__height = value
+
+    @property
+    def get_x(self):
+        return self.__x
+
+    def set_x(self, value):
+        self.__x = value
+
+    @property
+    def get_y(self):
+        return self.__y
+
+    def set_y(self, value):
+        self.__y = value
