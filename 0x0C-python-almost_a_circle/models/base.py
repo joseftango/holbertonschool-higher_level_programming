a89770365df6b98d0e39489f941252e8c8ed171c
joseftango <4279@holbertonschool.com>
2022-06-07 12:14:26 +0100

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""this is a class"""
-
 
 class Base:
+    """Base class
+    this is the class for the Base
+    """
     __nb_objects = 0
     def __init__(self, id=None):
-
+        """__init__ that initialize instance attribute """
         if id is None:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
