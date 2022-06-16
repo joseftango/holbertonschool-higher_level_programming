2d01f93c126894119e0e1a514c09bf88edb1aa13
joseftango <4279@holbertonschool.com>
2022-06-16 01:51:05 +0100

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,6 +18,7 @@ class Base:
         else:
             self.id = id
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """method that return a json string representation"""
         if list_dictionaries is None:
