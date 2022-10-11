d8dd75781d76c9f406ae3b8733af925e111b1b48
joseftango <4279@holbertonschool.com>
2022-10-11 20:16:48 +0100

    circular singly linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+* check_cycle - checks if a singly linked list has a cycle in it
+* @list: the given list
+* Return: 1 or 0
+**/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *traversal = list->next;
+
+	if (!list)
+		return (0);
+
+	while (traversal != NULL)
+	{
+		if (traversal == list)
+			return (1);
+		traversal = traversal->next;
+	}
+
+	return (0);
+}
