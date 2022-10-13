772ce6e5899f82c6ce143b6ba4d9a4067ee2ac77
joseftango <4279@holbertonschool.com>
2022-10-13 11:37:28 +0100

    13-insert_number.c

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,56 @@
+#include "lists.h"
+/**
+* insert_node - inserts a number into a sorted linked list
+* @head: double pointer to a list
+* @number: integer
+* Return: address of the new node
+**/
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *traversal, *pre_traversal, *new_node;
+
+	if (!head)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = number;
+	new_node->next = NULL;
+
+	if (!(*head))
+	{
+		new_node->n = number;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	traversal = (*head)->next;
+	pre_traversal = *head;
+
+	while (traversal)
+	{
+		if (traversal->n > new_node->n)
+		{
+			new_node->next = traversal;
+			pre_traversal->next = new_node;
+			break;
+		}
+
+		if (pre_traversal->n > new_node->n)
+		{
+			new_node->next = pre_traversal;
+			break;
+		}
+
+		traversal = traversal->next;
+		pre_traversal = pre_traversal->next;
+
+	}
+
+	return (new_node);
+}
