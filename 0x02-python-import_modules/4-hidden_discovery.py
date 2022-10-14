827634fca917a2b7b28c9fc8e93a206c6d861382
joseftango <4279@holbertonschool.com>
2022-10-14 17:06:27 +0100

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == '__main__':
-	import hidden_4
+    import hidden_4
 
 names = dir(hidden_4)
 
 for item in names:
-	if item[0] != '_' and item[1] != '_':
-		print(f"item")
+    if item[0] != '_' and item[1] != '_':
+        print(f"item")
