b6f240b49848bf259286a3b14fe49e0af61b658e
joseftango <4279@holbertonschool.com>
2022-10-23 12:14:33 +0100

    4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""(__import__("4-print_square").__doc__)"""
+"""4-print_square = __import__("4-print_square").__doc__)"""
+
+
 def print_square(size):
+    """function that prints a square with #"""
     if type(size) != int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
+
     for i in range(size):
         for j in range(size):
             print("#", end="")
