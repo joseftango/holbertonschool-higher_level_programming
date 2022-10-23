99a8f836a48dbfa3cfde8edbbcdb463844c1ae14
joseftango <4279@holbertonschool.com>
2022-10-23 13:23:08 +0100

    5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,9 +1,60 @@
 #!/usr/bin/python3
-"""__import__('5-text_indentation).text_indentation"""
+"""5-text_indentation = __import__(5-text_indentation).__doc__"""
+
+
 def text_indentation(text):
-    for i in text:
-        if i == '.' or i == '?' or i == ':':
-            print(f"{i}", end="")
-            print("\n")
-        else:
-            print(f"{i}", end="")
+	"""
+	>>> text_indentation(""Lorem ipsum dolor sit amet: consectetur adipiscing elit.\
+	...	Quonam modo?first solve the problem:then write.the code?"")
+	Lorem ipsum dolor sit amet:
+	<BLANKLINE>
+	consectetur adipiscing elit.
+	<BLANKLINE>
+	Quonam modo?
+	<BLANKLINE>
+	first solve the problem:
+	<BLANKLINE>
+	then write.
+	<BLANKLINE>
+	the code?
+	<BLANKLINE>
+
+	>>> text_indentation(":?.")
+	:
+	<BLANKLINE>
+	?
+	<BLANKLINE>
+	.
+	<BLANKLINE>
+	>>> text_indentation("10:15?20.")
+	10:
+	<BLANKLINE>
+	15?
+	<BLANKLINE>
+	20.
+	<BLANKLINE>
+	>>> text_indentation("")
+	>>> text_indentation(55555)
+	Traceback (most recent call last):
+		...
+	TypeError: text must be a string
+
+	>>> text_indentation(None)
+	Traceback (most recent call last):
+		...
+	TypeError: text must be a string
+
+	>>> text_indentation()
+	Traceback (most recent call last):
+		...
+	TypeError: text_indentation() missing 1 required positional argument: 'text'
+	"""
+	if type(text) is not str:
+		raise TypeError("text must be a string")
+	for c in text:
+		print(c, end="")
+		if c in [".","?", ":"]:
+			print("\n")
+	"""function that prints a text with
+	2 new lines after a specific character"""
+	
