8f26a4e3f46c0bdcdc4fd3156692c4d697589684
joseftango <4279@holbertonschool.com>
2022-11-08 00:01:23 +0100

    7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
---  inserts a new row in the table first_table
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'BestSchool');
+-- that inserts a new row in the table
+INSERT INTO first_table(id, name)
+VALUES(89, "Best School")
