2e4f733ff7bc70c075d3fc1c1b114ede3ccdb264
joseftango <4279@holbertonschool.com>
2023-04-25 00:53:53 +0100

    9-add.js

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const process = require('process');
-const num1 = parseInt(process.argv[2]), num2 = parseInt(process.argv[3]);
+const num1 = parseInt(process.argv[2]); const num2 = parseInt(process.argv[3]);
 
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
 
