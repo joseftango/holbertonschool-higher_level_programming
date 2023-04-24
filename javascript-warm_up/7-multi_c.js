55e27fb9543a8ccc4149d3d796273a444dcea9d6
joseftango <4279@holbertonschool.com>
2023-04-24 18:09:52 +0100

    7-multi_c.js

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const process = require('process');
+const x = process.argv[2];
+let i = 0;
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
+}
