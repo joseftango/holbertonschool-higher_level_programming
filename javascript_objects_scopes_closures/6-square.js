9e78b49b0c3daea3a95b6555d2b93897213697a5
joseftango <4279@holbertonschool.com>
2023-04-27 12:41:06 +0100

    6-square.js

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square');
 
 class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
     if (c === undefined) { c = 'X'; }
     for (let i = 0; i < this.height; i++) {
