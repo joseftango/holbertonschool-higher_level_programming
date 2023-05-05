13d3a5efb4296514f929901ba9bd76da7e3a81c1
joseftango <4279@holbertonschool.com>
2023-05-05 11:23:29 +0100

    102-concat.js

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -3,5 +3,5 @@ const fs = require('fs');
 
 const file1 = fs.readFileSync(process.argv[2]).toString();
 const file2 = fs.readFileSync(process.argv[3]).toString();
-const file3 = process.argv[4]
+const file3 = process.argv[4];
 fs.writeFileSync(file3, file1 + file2);
