759ff318b57aa25bb5172b82321dedd8216f0868
joseftango <4279@holbertonschool.com>
2023-07-10 14:58:10 +0100

    100-starwars_characters.js

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,27 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-const film = process.argv[2];
-let url = 'http://swapi.co/api/people/';
-function filmcharacters (film, url) {
-  request(url, function (err, response, body) {
-    if (err) {
-      console.log(err);
-    } else if (response.statusCode === 200) {
-      let jsonobj = JSON.parse(body);
-      let people = jsonobj.results;
-      for (let i in people) {
-        for (let j in people[i].films) {
-          if (people[i].films[j].includes(film)) {
-            console.log(people[i].name);
-          }
-        }
-      }
-      if (jsonobj.next !== null) {
-        filmcharacters(film, jsonobj.next);
-      }
-    } else {
-      console.log('An error occured. Status code: ' + response.statusCode);
+const address = 'https://swapi.co/api/films/' + process.argv[2];
+
+request(address, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const results = JSON.parse(body).characters;
+    for (const i of results) {
+      request(i, (e, r, b) => console.log(JSON.parse(b).name));
     }
-  });
-}
-filmcharacters(film, url);
+  }
+});
