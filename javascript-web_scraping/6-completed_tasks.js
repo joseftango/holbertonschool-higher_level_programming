ab5720d519c8c7bbc7197c7c9646ccb73ec00c1c
joseftango <4279@holbertonschool.com>
2023-07-12 15:54:26 +0100

    6-completed_tasks.js

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,19 +1,27 @@
 #!/usr/bin/node
-const process = require('process');
+
 const request = require('request');
+
 const url = process.argv[2];
 
-request(url, function (error, response, body) {
-  if (error) { throw console.log(error); }
-  const obj = JSON.parse(body);
-  let count;
-  const res = {};
-  for (let i = 0; i < obj.length; i++) {
-    if (res[`${obj[i].userId}`] === undefined) {
-      count = 0;
-    }
-    if (obj[i].completed === true) { count++; }
-    res[`${obj[i].userId}`] = count;
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
   }
-  console.log(res);
+
+  const todos = JSON.parse(body);
+  const completedByUser = {};
+
+  todos.forEach((todo) => {
+    if (todo.completed) {
+      if (completedByUser[todo.userId]) {
+        completedByUser[todo.userId] += 1;
+      } else {
+        completedByUser[todo.userId] = 1;
+      }
+    }
+  });
+
+  console.log(completedByUser);
 });
