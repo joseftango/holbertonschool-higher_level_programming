9ceb48e6ef21bcb0e84b00084b5dbdd3c08f49cc
joseftango <4279@holbertonschool.com>
2023-10-03 07:05:03 +0100

    2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 class Rectangle {
-	constructor(w, h) {
-		if ((w <= 0 || w == undefined) || (h <= 0 || h == undefined)) {
-		this.width;
-		this.height; }
-		else {
-			this.width = w;
-			this.height = h; } 
-	}
+  constructor (w, h) {
+    if ((w <= 0 || w == undefined) || (h <= 0 || h == undefined)) {
+      this.width;
+      this.height;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
 }
 
 module.exports = Rectangle;
