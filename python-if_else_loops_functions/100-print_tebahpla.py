4d84af6d01c0fe590277e6f538edabb7d37e82e4
joseftango <4279@holbertonschool.com>
2024-09-07 13:53:35 +0100

    100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 for j in range(90, 64, -1):
     if j % 2 == 0:
-        print('{}'.format(chr(j)), end='')
-        continue
-    j += 32
+        j += 32
     print('{}'.format(chr(j)), end='')
