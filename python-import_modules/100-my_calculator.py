2fbf937578b40d227d45dde5f992d3d1e28bc987
joseftango <4279@holbertonschool.com>
2024-09-10 06:22:23 +0100

    100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -10,15 +10,16 @@ num1 = int(argv[1])
 num2 = int(argv[3])
 op = argv[2]
 
-if op in '+-*/':
-    if op == '+':
-        print('{} {} {} = {}'.format(num1, op, num2, add(num1, num2)))
-    elif op == '-':
-        print('{} {} {} = {}'.format(num1, op, num2, sub(num1, num2)))
-    elif op == '*':
-        print('{} {} {} = {}'.format(num1, op, num2, mul(num1, num2)))
+if __name__ == '__main__':
+    if op in '+-*/':
+        if op == '+':
+            print('{} {} {} = {}'.format(num1, op, num2, add(num1, num2)))
+        elif op == '-':
+            print('{} {} {} = {}'.format(num1, op, num2, sub(num1, num2)))
+        elif op == '*':
+            print('{} {} {} = {}'.format(num1, op, num2, mul(num1, num2)))
+        else:
+            print('{} {} {} = {}'.format(num1, op, num2, div(num1, num2)))
     else:
-        print('{} {} {} = {}'.format(num1, op, num2, div(num1, num2)))
-else:
-    print('Unknown operator. Available operators: +, -, * and /')
-    exit(1)
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
