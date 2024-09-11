a853a9515af921ddcef8ce478af55501bc4861f4
joseftango <4279@holbertonschool.com>
2024-09-11 11:02:09 +0100

    2-uniq_add.py

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list == [] or my_list == None:
+    if my_list == [] or my_list is None:
         return 0
     my_list = list(set(my_list))
     acc = my_list[0]
