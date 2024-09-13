fa07ba1b6388ddc43ee88a3dbc197b2b71f0e052
joseftango <4279@holbertonschool.com>
2024-09-13 13:29:59 +0100

    0-safe_print_list.py

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for e in my_list:
+            print(e, end='')
+            count += 1
+        print()
+    except IndexError:
+        pass
+    return count
