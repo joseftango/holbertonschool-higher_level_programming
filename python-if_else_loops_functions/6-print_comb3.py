f9965f2e7479490c1c339e92f459844ed241a249
joseftango <4279@holbertonschool.com>
2024-11-27 05:40:17 +0100

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-for x in range(0, 99):
-    if x % 10 > x / 10:
-        if x != 89:
-            print("{:02d}, ".format(x), end='')
+i = 0
+while i < 100:
+    if i / 10 < i % 10:
+        if i == 89:
+            print('{:0=2d}'.format(i))
         else:
-            print("{:02d}".format(x))
+            print('{:0=2d}'.format(i), end=', ')
+    i += 1
