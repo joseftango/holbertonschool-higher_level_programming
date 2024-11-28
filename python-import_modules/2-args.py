23716126324d47eec7ed27290b72fba08c759240
joseftango <4279@holbertonschool.com>
2024-11-28 08:42:37 +0100

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 from sys import argv
-num_of_args = len(argv[1:])
-args = argv[1:]
 
 
-if __name__ == '__main__':
-    if num_of_args > 1:
-        print('{} arguments:'.format(num_of_args))
-        for i in range(num_of_args):
-            print('{}: {}'.format(i + 1, args[i]))
-    elif num_of_args < 1:
-        print('0 arguments.')
-    else:
-        print("1 argument:")
-        print("1: {}".format(args[0]))
+num_of_args = len(argv) - 1
+
+if num_of_args == 0:
+    print('{} arguments.'.format(num_of_args))
+elif num_of_args == 1:
+    print('{} argument:'.format(num_of_args))
+    print('1: {}'.format(argv[1]))
+else:
+    print('{} arguments:'.format(num_of_args))
+    for i in range(1, num_of_args + 1):
+        print('{}: {}'.format(i, argv[i]))
