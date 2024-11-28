6e7a0eccff992d4456722e72f0a5600a5f8bebf3
joseftango <4279@holbertonschool.com>
2024-11-28 02:14:19 +0100

    100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-for j in range(90, 64, -1):
-    if j % 2 == 0:
-        j += 32
-    print('{}'.format(chr(j)), end='')
+ord_chr = 122
+i = 0
+while i < 26:
+    print('{}'.format(chr(ord_chr)), end='')
+    if ord_chr >= 97 and ord_chr <= 122:
+        ord_chr -= 32
+    else:
+        ord_chr += 32
+    ord_chr -= 1
+    i += 1
