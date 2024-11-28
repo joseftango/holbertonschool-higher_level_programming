606ea421bbc81b933244c24e6c7fe7d956fe86de
joseftango <4279@holbertonschool.com>
2024-11-28 08:58:19 +0100

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 from sys import argv
 
-num_of_args = len(argv) - 1
-res = 0
 
-if num_of_args == 0:
-    print('{}'.format(num_of_args))
-elif num_of_args == 1:
-    print('{}'.format(num_of_args))
-else:
-    for i in range(1, num_of_args + 1):
-        res += int(argv[i])
-    print('{}'.format(res))
+if __name__ == '__main__':
+    num_of_args = len(argv) - 1
+    res = 0
+
+    if num_of_args == 0:
+        print('{}'.format(num_of_args))
+    elif num_of_args == 1:
+        print('{}'.format(num_of_args))
+    else:
+        for i in range(1, num_of_args + 1):
+            res += int(argv[i])
+        print('{}'.format(res))
