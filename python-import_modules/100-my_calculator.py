5ad5ec2a824acb03adbfe99f83861a7909f90954
joseftango <4279@holbertonschool.com>
2024-11-29 01:21:56 +0100

    100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-from calculator_1 import add, mul, sub, div
+from calculator_1 import add, sub, mul, div
 from sys import argv
-num_of_args = len(argv[1:])
-if num_of_args != 3:
-    print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-    exit(1)
 
-num1 = int(argv[1])
-num2 = int(argv[3])
-op = argv[2]
 
 if __name__ == '__main__':
-    if op in '+-*/':
-        if op == '+':
-            print('{} {} {} = {}'.format(num1, op, num2, add(num1, num2)))
-        elif op == '-':
-            print('{} {} {} = {}'.format(num1, op, num2, sub(num1, num2)))
-        elif op == '*':
-            print('{} {} {} = {}'.format(num1, op, num2, mul(num1, num2)))
-        else:
-            print('{} {} {} = {}'.format(num1, op, num2, div(num1, num2)))
-    else:
+    num_of_args = len(argv) - 1
+    if num_of_args != 3:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    op = str(argv[2])
+    ops = {'+': add, '-': sub, '*': mul, '/': div}
+
+    if op not in ops.keys():
         print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
+
+    print('{} {} {} = {}'.format(a, op, b, ops[op](a, b)))
