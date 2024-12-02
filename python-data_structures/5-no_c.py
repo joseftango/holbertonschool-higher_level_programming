30757fa169a7961ec2d1738b5a7c2882321ab083
joseftango <4279@holbertonschool.com>
2024-12-02 04:26:46 +0100

    5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_str = ''
-    for i in range(len(my_string)):
-        if my_string[i] in 'cC':
-            continue
-        new_str += my_string[i]
+    for c in my_string:
+        if c not in 'cC':
+            new_str += c
     return new_str
