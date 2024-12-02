9a156f697390118a8ad8a917607d69e60a0eaa5e
joseftango <4279@holbertonschool.com>
2024-12-02 06:04:09 +0100

    7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
+def valid_tuple(tup=()):
+    if len(tup) < 2:
+        if len(tup) == 1:
+            valid_tup = tup[0], 0
+        else:
+            valid_tup = 0, 0
+
+    elif len(tup) >= 2:
+        valid_tup = tup[0], tup[1]
+
+    return valid_tup
+
+
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
-        tuple_a += (0, 0)
-    if len(tuple_b) < 2:
-        tuple_b += (0, 0)
-    tuple_c = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-    return tuple_c
+
+    valid_tuple_a = valid_tuple(tuple_a)
+    valid_tuple_b = valid_tuple(tuple_b)
+
+    res1 = valid_tuple_a[0] + valid_tuple_b[0]
+    res2 = valid_tuple_a[1] + valid_tuple_b[1]
+
+    return res1, res2
