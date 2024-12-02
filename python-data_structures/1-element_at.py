cf373141feee0900c7269cfe62d418ee781a00c3
joseftango <4279@holbertonschool.com>
2024-12-02 03:46:34 +0100

    1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list) - 1:
+    max_idx = len(my_list) - 1
+    if idx < 0:
+        return None
+    if idx > max_idx:
         return None
     return my_list[idx]
