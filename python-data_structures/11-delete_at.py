904dd29bb2bdefb471b5b3aff6d7bdc524582917
joseftango <4279@holbertonschool.com>
2024-12-02 06:52:26 +0100

    11-delete_at.py

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    max_idx = len(list) - 1
+    max_idx = len(my_list) - 1
     if idx > max_idx or idx < 0:
         return my_list
     del my_list[idx]
