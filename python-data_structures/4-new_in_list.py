a79eef602bd1ad9bbf33c927c7d1c298f9b2a6d6
joseftango <4279@holbertonschool.com>
2024-12-02 04:11:58 +0100

    4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,8 +3,8 @@ def new_in_list(my_list, idx, element):
     max_idx = len(my_list) - 1
     new_li = my_list[:]
     if idx < 0:
-        return my_list
+        return new_li
     if idx > max_idx:
-        return my_list
+        return new_li
     new_li[idx] = element
     return new_li
