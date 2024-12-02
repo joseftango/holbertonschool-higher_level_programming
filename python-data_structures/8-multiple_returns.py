759c74926c3e2e3b41623b2450408eadcf7ad19f
joseftango <4279@holbertonschool.com>
2024-12-02 06:13:35 +0100

    8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == '':
-        return None, len(sentence)
+    if sentence == '' or sentence == None:
+        return None, 0
     return len(sentence), sentence[0]
