170619e942644bb91a70fcc94398b134ec169270
joseftango <4279@holbertonschool.com>
2024-12-02 03:55:32 +0100

    2-replace_in_list.py

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,8 +2,8 @@
 def replace_in_list(my_list, idx, element):
     max_idx = len(my_list) - 1
     if idx < 0:
-        return None
+        return my_list
     if idx > max_idx:
-        return None
+        return my_list
     my_list[idx] = element
     return my_list
