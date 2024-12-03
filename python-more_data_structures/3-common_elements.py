01a57b70b55dbd824877271c73cd2290f88827c6
joseftango <4279@holbertonschool.com>
2024-12-03 10:45:52 +0100

    3-common_elements.py

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     return set_1 & set_2
-    #return [i for i in set_1 if i in set_2]
+
+
+# def common_elements(set_1, set_2):
+# return [i for i in set_1 if i in set_2]
