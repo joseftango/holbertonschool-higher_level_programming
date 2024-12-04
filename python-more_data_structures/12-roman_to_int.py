7568b6e28585750be2cd8197b19dc9948afb997e
joseftango <4279@holbertonschool.com>
2024-12-04 05:55:23 +0100

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -14,37 +14,26 @@ def value(r):
         return 500
     if (r == 'M'):
         return 1000
-    return -1
+    return 0
 
 
 def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) == 'str':
+        return 0
+
     res = 0
     i = 0
 
-    if type(roman_string) is not str or roman_string is None:
-        return 0
-
     while (i < len(roman_string)):
-
-        # Getting value of symbol s[i]
         s1 = value(roman_string[i])
 
         if (i + 1 < len(roman_string)):
-
-            # Getting value of symbol s[i + 1]
             s2 = value(roman_string[i + 1])
 
-            # Comparing both values
             if (s1 >= s2):
-
-                # Value of current symbol is greater
-                # or equal to the next symbol
                 res = res + s1
                 i = i + 1
             else:
-
-                # Value of current symbol is greater
-                # or equal to the next symbol
                 res = res + s2 - s1
                 i = i + 2
         else:
