a4cf4e03de41430559755f07fbe2de1250d702ab
joseftango <4279@holbertonschool.com>
2024-12-05 09:41:03 +0100

    101-safe_function.py

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -4,8 +4,8 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        res = fct(args[0], args[1])
-    except (ValueError, TypeError, IndexError, ZeroDivisionError) as e:
+        res = fct(*args)
+    except Exception as e:
         print('Exception: {}'.format(e), file=sys.stderr)
         res = None
     return res
