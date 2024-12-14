d9ddcb3d216de51e5dd04eaa9d3340bff4d684c5
joseftango <4279@holbertonschool.com>
2024-12-14 09:58:08 +0100

    3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,10 +3,10 @@
 
 
 def say_my_name(first_name, last_name=""):
-	"""function that prints a full name"""
-	if type(first_name) is not str:
-		raise TypeError('first_name must be a string')
-	if type(last_name) is not str:
-		raise TypeError('last_name must be a string')
+    """function that prints a full name"""
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+    if type(last_name) is not str:
+        raise TypeError('last_name must be a string')
 
-	print(f'My name is {first_name} {last_name}')
+    print(f'My name is {first_name} {last_name}')
