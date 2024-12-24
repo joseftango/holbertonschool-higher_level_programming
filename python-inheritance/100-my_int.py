3e8fca1ecf27540c7215b39cb0ef2986202ef5e5
joseftango <4279@holbertonschool.com>
2024-12-24 23:53:16 +0100

    100-my_int.py

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """100-my_int module"""
 
-class MyInt(int):
-	'''MyInt class'''
-	pass
 
-	def __eq__(self, value):
-		return super().__ne__(value)
-	
-	def __ne__(self, value):
-		return super().__eq__(value)
+class MyInt(int):
+    '''MyInt class'''
+    pass
+
+    def __eq__(self, value):
+        return super().__ne__(value)
+
+    def __ne__(self, value):
+        return super().__eq__(value)
