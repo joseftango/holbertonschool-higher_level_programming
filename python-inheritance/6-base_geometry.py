fa06cd88c38df6e2e6ffa027857fa80aa326a9e9
joseftango <4279@holbertonschool.com>
2024-12-24 21:08:09 +0100

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''5-base_geometry module'''
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def area(self):
+        '''raises an Exception with message'''
+        raise Exception('area() is not implemented')
