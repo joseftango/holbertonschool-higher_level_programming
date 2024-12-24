ce1dece5d11d816f3ec1830617086301196a67e2
joseftango <4279@holbertonschool.com>
2024-12-24 23:41:01 +0100

    11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''10-square module'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''class Square'''
+    def __init__(self, size):
+        super().integer_validator('size', size)
+        super().__init__(size, size)
