de0b174ac24d7965554504e392ea2039dd455aa3
joseftango <4279@holbertonschool.com>
2024-12-26 15:17:45 +0100

    0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''0-read_file module'''
+
+
+def read_file(filename=""):
+	'''function that reads a text file
+	(UTF8) and prints it to stdout'''
+	with open(filename, 'r', encoding='UTF-8') as f:
+		print(f.read(), end='')
