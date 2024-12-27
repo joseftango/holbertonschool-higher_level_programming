1ade5a268d4c6026ef7761f83d3358f2498f6966
joseftango <4279@holbertonschool.com>
2024-12-27 21:06:44 +0100

    12-pascal_triangle.py

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''12-pascal_triangle module'''
+from math import factorial
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers
+    representing the Pascal's triangle"""
+    if n <= 0:
+        return []
+    res = [[1]]
+
+    for i in range(n - 1):
+        temp = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(res[-1]) + 1):
+            row.append(temp[j] + temp[j + 1])
+        res.append(row)
+
+    return res
