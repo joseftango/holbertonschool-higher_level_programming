74caf8ed3cfc6e009824c07686d18bcb08a46f2a
joseftango <4279@holbertonschool.com>
2025-01-02 15:49:10 +0100

    tests

---
+++ python-almost_a_circle/tests/test_rectangle_3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import unittest
+from models.rectangle import Rectangle
+
+
+class Test_Rectangle(unittest.TestCase):
+
+    def test_rectangle_width_and_height_exceptions(self):
