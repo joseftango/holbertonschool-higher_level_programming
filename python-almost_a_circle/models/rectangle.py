778d514eb8dcb6fbad5406a4e19f0cfdd65dcec9
joseftango <4279@holbertonschool.com>
2025-01-02 17:19:24 +0100

    models/rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -74,5 +74,5 @@ class Rectangle(Base):
             print()
 
     def __str__(self):
-        return f'{self.__class__.__name__} ({self.id})\
+        return f'[{self.__class__.__name__}] ({self.id})\
  {self.x}/{self.y} - {self.width}/{self.height}'
