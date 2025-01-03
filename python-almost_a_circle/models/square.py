0c2948e9508259f06612668c1998bd49b635d090
joseftango <4279@holbertonschool.com>
2025-01-03 05:01:07 +0100

    models/square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -20,3 +20,17 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        '''assigns an args and kwargs
+        to the respective attributes'''
+        if args and len(args) > 0:
+            my_keys = ['id', 'size', 'x', 'y']
+            i = 0
+            while i in range(len(args)):
+                for k in my_keys[:len(args)]:
+                    setattr(self, k, args[i])
+                    i += 1
+        else:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
