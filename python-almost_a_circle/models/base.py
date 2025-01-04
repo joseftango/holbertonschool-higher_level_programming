27732e93648c6fc055ef81c5e74392f052c95cdf
joseftango <4279@holbertonschool.com>
2025-01-04 06:00:15 +0100

    models/base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,15 +2,12 @@
 '''base module'''
 import json
 from . import rectangle
-'''import rectangle model'''
 
 
 class Base:
-    '''Base class'''
     __nb_objects = 0
 
     def __init__(self, id=None):
-        '''constractor method'''
         if id is not None:
             self.id = id
         else:
@@ -19,16 +16,12 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        '''returns the JSON string
-        representation of list_dictionaries'''
         if list_dictionaries in [None, []]:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        '''writes the JSON string representation
-        of list_objs to a file'''
         if list_objs in [None, []]:
             with open(f'{cls.__name__}.json', 'w', encoding='UTF-8') as f:
                 f.write('[]')
@@ -46,16 +39,12 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        '''returns the list of the JSON
-        string representation json_string'''
         if json_string in [None, '[]']:
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        '''returns an instance with
-        all attributes already set'''
         my_obj = rectangle.Rectangle(10, 10)
         my_obj.update(**dictionary)
         return my_obj
