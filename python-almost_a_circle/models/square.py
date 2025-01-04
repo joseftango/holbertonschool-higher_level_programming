0767197ad01a41fe8f3441659c8373db9048589c
joseftango <4279@holbertonschool.com>
2025-01-04 05:55:54 +0100

    models/square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 '''square module'''
-from models.rectangle import Rectangle
+from models import rectangle
 
 
-class Square(Rectangle):
+class Square(rectangle.Rectangle):
     '''Square class inherits from rectangle'''
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
