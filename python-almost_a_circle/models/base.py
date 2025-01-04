1b4fb1e5cecabcf29cfae5eede6959e21b13b33c
joseftango <4279@holbertonschool.com>
2025-01-04 06:16:27 +0100

    models/base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""Base module
-defines the base class for Rectangle and Square to inherit from
-"""
-import json
-from . import rectangle
+'''base module'''
+from json import dumps, loads
 
 
 class Base:
@@ -11,6 +8,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        '''constractor method'''
         if id is not None:
             self.id = id
         else:
@@ -19,12 +17,16 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        '''returns the JSON string
+        representation of list_dictionaries'''
         if list_dictionaries in [None, []]:
             return "[]"
-        return json.dumps(list_dictionaries)
+        return dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
+        '''writes the JSON string representation
+        of list_objs to a file'''
         if list_objs in [None, []]:
             with open(f'{cls.__name__}.json', 'w', encoding='UTF-8') as f:
                 f.write('[]')
@@ -42,12 +44,16 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
+        '''returns the list of the JSON
+        string representation json_string'''
         if json_string in [None, '[]']:
             return []
-        return json.loads(json_string)
+        return loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        dummy = rectangle.Rectangle(10, 10)
-        dummy.update(**dictionary)
-        return dummy
+        '''returns an instance with
+        all attributes already set'''
+        my_obj = cls(10, 10)
+        my_obj.update(**dictionary)
+        return my_obj
