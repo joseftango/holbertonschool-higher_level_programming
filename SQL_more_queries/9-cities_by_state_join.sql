181c8e37fe38734613dcc8e148f0942cdef5701e
joseftango <4279@holbertonschool.com>
2025-02-02 01:09:23 +0100

    9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name from cities 
+SELECT cities.id, cities.name, states.name FROM cities 
 INNER JOIN states
 WHERE cities.state_id = states.id;
