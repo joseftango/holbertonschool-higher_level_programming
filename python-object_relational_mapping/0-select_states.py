70aaa1296a0866cfeae17ebcaff2ec9aaf3fdd96
joseftango <4279@holbertonschool.com>
2025-02-07 22:34:39 +0100

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
 '''0-select_states module'''
 from sys import argv
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker, declarative_base
+import MySQLdb
 
 
 username = argv[1]
 password = argv[2]
 database = argv[3]
 
-DATABASE_URL = "mysql+mysqldb://{}:{}@localhost/{}".format(username, password, database)
-engine = create_engine(DATABASE_URL)
+connection = MySQLdb.connect(host='localhost', user=username,
+                             passwd=password, db=database)
+cursor = connection.cursor()
 
-Base = declarative_base()
+cursor.execute('SELECT * FROM states')
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
 
-class State(Base):
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(255), nullable=False)
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
-states = session.query(State).all()
-
-for state in states:
-    print(f'({state.id}, {state.name})')
-
-session.close()
+cursor.close()
+connection.close()
