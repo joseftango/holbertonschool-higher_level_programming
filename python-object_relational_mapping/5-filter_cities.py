658b9c2639a70b8f42a2f6117ae5756d85f68a42
joseftango <4279@holbertonschool.com>
2025-02-08 15:10:32 +0100

    5-filter_cities.py

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''3-my_safe_filter_states module'''
+from sys import argv
+import MySQLdb
+
+
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state_name = argv[4]
+
+    connection = MySQLdb.connect(host='localhost',
+                                 port=3306,
+                                 user=username,
+                                 passwd=password,
+                                 db=database)
+
+    cursor = connection.cursor()
+    cursor.execute('SELECT name FROM cities\
+                    WHERE cities.state_id =\
+                    (SELECT id FROM states WHERE name = "{}")\
+                    ORDER BY cities.id ASC;'.format(state_name))
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print('{}, '.format(*row), end='')
+    print()
+
+    cursor.close()
+    connection.close()
