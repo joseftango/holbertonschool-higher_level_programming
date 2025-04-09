372e3a58327b8fff4d1fee46a1e7c7719b94c458
joseftango <4279@holbertonschool.com>
2025-04-09 19:05:52 +0100

    11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-const process = require('process');
-const num = process.argv;
-if (num.length === 2 || num.length === 3) {
-  console.log(0);
-} else {
-  num.sort((a, b) => a - b);
-  console.log(num[num.length - 2]);
+const args = process.argv.slice(2);
+const myList = args.map(num => parseInt(num));
+
+if (myList.length === 0) { console.log(0); } else if (myList.length === 1) { console.log(1); } else {
+  let largest = myList[0];
+  for (let i = 0; i < myList.length; i++) {
+    if (myList[i] > largest) { largest = myList[i]; }
+  }
+  console.log(largest);
 }
