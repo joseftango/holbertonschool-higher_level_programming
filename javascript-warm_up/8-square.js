8020f9ba09e0166ac6b0ea93e06d49bce1ab11a5
joseftango <4279@holbertonschool.com>
2025-04-09 17:03:38 +0100

    8-square.js

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,18 +1,14 @@
 #!/usr/bin/node
-const process = require('process');
-const size = process.argv[2];
-let i = 0;
-let j;
-if (!isNaN(size)) {
-  while (i < size) {
-    j = 0;
-    while (j < size) {
-      process.stdout.write('X');
-      j++;
+const x = parseInt(process.argv[2]) || null;
+let square = '';
+if (x === null) { console.log('Missing size'); } else {
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j < x; j++) {
+      square += 'X';
     }
-    process.stdout.write('\n');
-    i++;
+    if (i < x - 1) {
+      square += '\n';
+    }
   }
-} else {
-  console.log('Missing size');
+  console.log(square);
 }
