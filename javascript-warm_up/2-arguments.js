e9867f01c691b85d898462a041c62f531f4328d3
joseftango <4279@holbertonschool.com>
2025-04-09 14:13:50 +0100

    2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-const process = require('process');
-
-const numargs = process.argv.length;
-
-if (numargs === 2) {
+const args = process.argv;
+const len = args.length;
+if (len === 2) {
   console.log('No argument');
-} else if (numargs === 3) {
+} else if (len === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
