1c073eb3455b9385e26b2408eb43ad1139434138
joseftango <4279@holbertonschool.com>
2025-04-09 16:34:24 +0100

    7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const x = parseInt(process.argv[2]) || null;
 if (x === null) {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-    for (let i=0; i<x; i++)
-        console.log('C is fun');
+  for (let i = 0; i < x; i++) { console.log('C is fun'); }
 }
