210fa7752b02be62bf4cf5c818026c06d7778d63
joseftango <4279@holbertonschool.com>
2025-04-09 16:13:15 +0100

    5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 const ar = parseInt(args[0]) || null;
-if (ar === null) { console.log('Not a Number'); } else { console.log(ar); }
+if (ar === null) { console.log('Not a Number'); } else { console.log('My number: ' + ar); }
