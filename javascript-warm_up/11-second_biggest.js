d396935009b95e791fa5b0a165619179b14cb071
joseftango <4279@holbertonschool.com>
2025-04-09 19:16:48 +0100

    11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 const args = process.argv.slice(2);
 const myList = args.map(num => parseInt(num)).sort((a, b) => b - a);
 
-if (myList.length === 0) { console.log(0); } else if (myList.length === 1) { console.log(1); } else {
+if (myList.length === 0 || myList.length === 1) { console.log(0); } else {
   const secondLargest = myList[1];
   console.log(secondLargest);
 }
