a08a1bee296abf974c03f90a8aabe7309a5fe953
joseftango <4279@holbertonschool.com>
2025-04-15 02:13:49 +0100

    100-map.js

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-let newli = [];
-newli = list.map(function (el, index) {
-  return el * index;
-});
 console.log(list);
-console.log(newli);
+const newList = list.map((element, index) => element * index);
+console.log(newList);
