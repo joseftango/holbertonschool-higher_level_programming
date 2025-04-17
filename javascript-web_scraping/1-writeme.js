b01e27af0540dd674186b4a53ea0327dbde9ad66
joseftango <4279@holbertonschool.com>
2025-04-17 18:39:32 +0100

    1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 const fp = process.argv[2];
-const str = process.argv[3] + '\n';
+const str = process.argv[3];
 
 fs.writeFile(fp, str, (err, data) => {
   if (err) { console.log(err); }
