c0376f947d3a584f66000ea05fb777f3feb55193
joseftango <4279@holbertonschool.com>
2025-04-18 23:48:18 +0100

    3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
-const process = require('process');
 const request = require('request');
 const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/';
-const wantedurl = url + id;
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 
-request(wantedurl, function (error, response, body) {
-  if (error) { throw console.log(error); }
-  const obj = JSON.parse(body);
-  console.log(obj.title);
+request.get({ url, json: true }, (err, res, body) => {
+  if (err) {
+    console.error('Error:', err);
+  } else if (res.statusCode !== 200) {
+    console.error('Failed with status code:', res.statusCode);
+  } else {
+    console.log(body.title);
+  }
 });
