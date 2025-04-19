4c115ee101afbc4348f9c3af81f42453e4ee6388
joseftango <4279@holbertonschool.com>
2025-04-19 02:36:46 +0100

    4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,22 @@
 #!/usr/bin/node
-const process = require('process');
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (error, response, body) {
-  if (error) { throw console.log(error); }
-  const obj = JSON.parse(body);
-  let count = 0;
-  for (const item in obj.results) {
-    const chars = obj.results[item].characters;
-    for (const i in chars) {
-      if (chars[i].includes('18')) { count++; }
+request.get({ url, json: true }, (err, res, body) => {
+  if (err) {
+    console.error('Error:', err);
+  } else if (res.statusCode !== 200) {
+    console.error('Failed with status code:', res.statusCode);
+  } else {
+    let count = 0;
+    for (const movie of body.results) {
+      for (const char of movie.characters) {
+        if (char.endsWith('/18/')) {
+          count += 1;
+          break;
+        }
+      }
     }
+    console.log(count);
   }
-  console.log(count);
 });
